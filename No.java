215b3698862d5a1a971c41b072e0948c069ecd0d
Joana Letícia <79266764+JoanaLeticia@users.noreply.github.com>
2022-06-17 11:24:33 -0300

    Add files via upload

---
+++ No.java
@@ -0,0 +1,15 @@
+package arvoreBuscaBinaria;
+
+public class No {
+	public int valor;
+	public No esquerda;
+	public No direita;
+	
+	// CONSTRUTOR
+	No(int valor) {
+		this.valor = valor;
+		this.esquerda = null;
+		this.direita = null;
+	}
+
+}
